38584b5d0c03ed61f9a19ebdc3afb80943d63a62
Justin Browne <jebdude89@gmail.com>
2019-08-22 01:26:41 -0400

    Change labels

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ module.exports.loop = function () {
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        creep.room.visual.text(`${creep.name} (${creep.memory.role})`, creep.pos, {font: "10px", opacity: 0.7, stroke: "black", strokeWidth: 0.07});
+        creep.room.visual.text(`${creep.name} (${creep.memory.role})`, creep.pos, {font: "10px", stroke: "black", strokeWidth: 0.08});
         if (creep.memory.role in roles && creep.memory.state != "ignore") {
             roles[creep.memory.role].run(creep);
         }
